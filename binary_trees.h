511a5fd9634e80716aada105b81b5fbca882df7f
KarenCampo777 <1472@holbertonschool.com>
2020-07-25 02:17:10 +0000

    deleted repeated prototypes

--- binary_trees.h
+++ binary_trees.h
@@ -42,14 +42,13 @@ void count(const binary_tree_t *node, size_t *s);
 size_t binary_tree_size(const binary_tree_t *tree);
 void count_nodes(const binary_tree_t *node, size_t *n);
 size_t binary_tree_height(const binary_tree_t *tree);
-size_t binary_tree_nodes(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
 void count_leaves(const binary_tree_t *node, size_t *l);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
-void count_nodes(const binary_tree_t *node, size_t *n);
+
 
 
 
