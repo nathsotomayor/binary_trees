2138f233d9a10d5b89dfab93e01c000e48776c57
KarenCampo777 <1472@holbertonschool.com>
2020-07-25 01:56:06 +0000

    updated header

--- binary_trees.h
+++ binary_trees.h
@@ -44,6 +44,13 @@ void count_nodes(const binary_tree_t *node, size_t *n);
 size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
+void count_leaves(const binary_tree_t *node, size_t *l);
+size_t binary_tree_leaves(const binary_tree_t *tree);
+size_t binary_tree_nodes(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
+int binary_tree_is_full(const binary_tree_t *tree);
+void count_nodes(const binary_tree_t *node, size_t *n);
+
 
 
 #endif /* BINARY_TREES */
